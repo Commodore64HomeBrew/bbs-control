a2d8dcb6abcf6655722d58a1ee390c612efea6c0
Kevin Casteels <kcasteels@gmail.com>
2016-06-16 06:51:05 +0000

    Added USB power control and echos

--- zoomfloppy/updatenews.sh
+++ zoomfloppy/updatenews.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
+hub-ctrl -h 0 -P 3 -p 1
+
+sleep 2
+
 sh cbcnews.sh
 
 sh 1571side.sh 1
 
 sh deletefile.sh "(bbs-l1)"
 
-#sh dirlist.sh
-
-#sh 1571side.sh 1
+echo "(bbs-l1) deleted"
 
 sh writefile.sh "(bbs-l1)"
 
-#sh dirlist.sh
+echo "(bbs-l1) written"
 
-
+hub-ctrl -h 0 -P 3 -p 0
 
 
 
