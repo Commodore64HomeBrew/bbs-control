e19be151434fc5e1f0d5c45843e65ca476adc20a
Kevin Casteels <kcasteels@gmail.com>
2016-07-05 17:45:40 +0000

    Working on formating

--- zoomfloppy/cbcnews.sh
+++ zoomfloppy/cbcnews.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
+FILEOUT="(bbs-l5)"
+
+RETURN=$(perl -C -e 'print chr 0x000d')
+
+RED=$(perl -C -e 'print chr 0xf101')
+GREEN=$(perl -C -e 'print chr 0xf10d')
+YELLOW=$(perl -C -e 'print chr 0xf10f')
+
+rm $FILEOUT
+rm topstories.xml
+
 wget http://rss.cbc.ca/lineup/topstories.xml
 
 sed -i '/<language>/ d' topstories.xml
 sed -i '/<lastBuildDate>/ d' topstories.xml
 sed -i '/<guid/ d' topstories.xml
-#sed -i ':a;N;$!ba;s/\n/^M/g' topstories.xml
 
-links -width 40 -dump topstories.xml | sed '/http/ d' | sed '/]]/ d' | tr 'a-zA-Z' 'A-Za-z' > "(bbs-l5)"
-#links -width 40 -dump topstories.xml | sed '/http/ d' | sed '/]]/ d' > topstories.txt
+#echo $RED'cbc nEWS rss fEED'$YELLOW > $FILEOUT
 
+links -width 40 -dump topstories.xml | sed '/http/ d' | sed '/]]/ d' | tr 'a-zA-Z' 'A-Za-z' >> $FILEOUT
+#links -width 40 -codepage en_US.UTF-8 -dump topstories.xml > $FILEOUT
 
-rm topstories.xml
+#sed -i ':a;N;$!ba;s/\n\n/'$RED'\n\n/2' $FILEOUT
 
+#sed -i ':a;N;$!ba;s/\n\n/'$GREEN'\n\n/g' $FILEOUT
+
+sed -i ':a;N;$!ba;s/\n/'$RETURN'/g' $FILEOUT
 
