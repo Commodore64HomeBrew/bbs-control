156bd02ebd75db77e966211bf329e4ca355a8fba
Kevin Casteels <kcasteels@gmail.com>
2016-07-26 16:43:04 -0700

    Added commodore news to daily update

--- updatenews.sh
+++ updatenews.sh
@@ -2,16 +2,19 @@
 
 bash cbcnews.sh
 bash nasanews.sh
+bash commodorenews.sh
 
 bash 1571side.sh 1
 
 bash deletefile.sh "(bbs-l1)"
 bash deletefile.sh "(bbs-l2)"
+bash deletefile.sh "(bbs-l3)"
 
 echo "Old files deleted"
 
 bash writefile.sh "(bbs-l1)"
 bash writefile.sh "(bbs-l2)"
+bash writefile.sh "(bbs-l3)"
 
 echo "New files written"
 
