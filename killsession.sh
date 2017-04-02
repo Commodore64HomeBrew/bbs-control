936c161cb2424f1145f455f9fa6126ea531571e7
Kevin Casteels <kcasteels@gmail.com>
2017-04-02 08:07:10 +0000

    ucbbs changes

--- killsession.sh
+++ killsession.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PID=$(ps -ef |grep bbs-session.sh | grep "sh bbs-session.sh" | awk -F ' ' '{print $2}')
+PID=$(ps -ef |grep ucbbs-session.sh | grep "sh ucbbs-session.sh" | awk -F ' ' '{print $2}')
 echo $PID
 kill $PID
 pkill tcpser
